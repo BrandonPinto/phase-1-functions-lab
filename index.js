7dd380617e7222fc03fe5146957adde1c2491d03
Brandon Pinto <b.pintony@gmail.com>
2022-07-19 10:19:15 -0400

    commit tree 1ceb8af7c2eb8e8f0770b0407f7950e6fbd2e798

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if(someValue < 42) {
+        return (42 - someValue);
+    } else {
+        return (someValue - 42);
+    }
+}
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+   return Math.abs(start - destination) *264
+//    const distanceTraveledInFeet = (start, destination) => math.abs(start - destination)
+}
+function calculatesFarePrice(start, destination) {
+    let traveled = distanceTravelledInFeet(start, destination)
+    if(traveled < 400) {
+    return 0
+} else if (traveled < 2000) {
+    return (traveled - 400)*.02
+} else if (traveled <= 2500) {
+    return 25
+} else {
+    return 'cannot travel that far'
+}
+}
+  //anon function const functionName = function(param, param) {
+    //stuff
+  //
